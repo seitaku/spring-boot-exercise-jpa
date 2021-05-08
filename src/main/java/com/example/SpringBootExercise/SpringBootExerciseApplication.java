3307f55731abef5b142ba12e00cb1170546f2d5c
Sei <seitaku@wethink.com.tw>
2021-05-09 02:05:37 +0800

    init

---
+++ src/main/java/com/example/SpringBootExercise/SpringBootExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootExercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootExercise/SpringBootExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootExercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
